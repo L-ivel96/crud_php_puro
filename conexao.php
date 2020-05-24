3e2651aec5962c6957696060b184a1631ff7b34a
LeviSiq <levi.siqueira@outlook.com>
2020-05-24 18:54:57 -0300

    Conexão com o bd e função de listar

---
+++ conexao.php
@@ -0,0 +1,65 @@
+<?php
+
+define('bd_host', 'localhost');
+define('bd_usuario', 'root');
+define('bd_senha', '');
+define('bd_nome', 'crud_php_puro');
+
+define('mysqli_con', true);
+
+function conectar()
+{
+	if(mysqli_con) {
+		$conexao = new mysqli(bd_host, bd_usuario, bd_senha, bd_nome);
+
+		if(!$conexao) {
+			echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		    exit;
+		}
+
+		return $conexao;
+	}
+}
+
+function fechar_conexao($con)
+{
+	if(mysqli_con) {
+		mysqli_close($con);
+	}
+}
+
+function consulta_bd($query)
+{
+	if(mysqli_con) {
+		$con = conectar();
+		$result = $con->query($query);
+		$result->fetch_all(MYSQLI_ASSOC);
+		fechar_conexao($con);
+
+		return $result;
+	}
+}
+
+function criar_banco_de_dados()
+{
+	return $bd =
+	//Cria bd
+	'CREATE SCHEMA `crud_php_puro` DEFAULT CHARACTER SET utf8 ; '.
+	//cria tabela clientes
+	'CREATE TABLE `crud_php_puro`.`cliente` (
+	  `id_cliente` INT NOT NULL AUTO_INCREMENT,
+	  `nome` VARCHAR(200) NULL,
+	  `cpf` VARCHAR(25) NULL,
+	  `email` VARCHAR(150) NULL,
+	  PRIMARY KEY (`id_cliente`))
+	ENGINE = InnoDB
+	DEFAULT CHARACTER SET = utf8; '.
+
+	"INSERT INTO `crud_php_puro`.`cliente` (`nome`, `cpf`, `email`) VALUES ('teste', '123456789', 'mail@mail.com'); ";
+	"INSERT INTO `crud_php_puro`.`cliente` (`nome`, `cpf`, `email`) VALUES ('teste 2', '123456789', 'mail2@mail.com'); ";
+	"INSERT INTO `crud_php_puro`.`cliente` (`nome`, `cpf`, `email`) VALUES ('teste 3', '123456789', 'mail3@mail.com'); ";
+	"INSERT INTO `crud_php_puro`.`cliente` (`nome`, `cpf`, `email`) VALUES ('teste 4', '123456789', 'mail4@mail.com'); ";
+
+
+}
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+	require_once('./conexao.php');
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Clientes</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light mb-2 d-flex" style="background-color: #e9ecef;">
+        <h2>CRUD Clientes</h2>
+    </nav>
+    <div class="container">
+
+	    <table id="tabela_clientes" class="table table-striped table-bordered">
+	        <thead class="thead-dark">
+	            <tr>
+	                <th>Nome</th>
+	                <th>CPF</th>
+	                <th>E-mail</th>
+	                <th>Editar</th>
+	                <th>Deletar</th>
+	            </tr>
+	        </thead>
+	        <tbody>
+	        	<?php 
+	        		$consulta = "SELECT * FROM cliente";
+	        		$clientes = consulta_bd($consulta);
+	        	?>
+	        	<?php foreach ($clientes as $cliente): ?>
+	        	<tr>
+	        		<td><?= $cliente["nome"]; ?></td>
+	        		<td><?= $cliente["cpf"]; ?></td>
+	        		<td><?= $cliente["email"]; ?></td>
+	        		<td>
+	        			<a class="btn btn-warning" href="./pagina_editar?op=editar&id=<?= $cliente['id_cliente'] ?>">Editar</a>
+	        		</td>
+	        		<td>
+	        			<a class="btn btn-danger" href="./index.php?op=excluir&id=<?= $cliente['id_cliente'] ?>">Excluir</a>
+	        		</td>
+	        	</tr>	        			
+	        	<?php endforeach?>
+	        </tbody>
+	        <tfoot>
+	            <tr>
+	                <td colspan="4"><a href="./pagina_cadastrar" class="btn btn-info">Adicionar Cliente</a></td>
+	            </tr>
+	        </tfoot>
+	    </table>
+	</div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
